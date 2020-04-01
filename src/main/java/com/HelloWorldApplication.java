43424ce6f10f60bbeca9b1e7776038387bc5eda2
Tommy Healy <healy.tomas@gmail.com>
2020-04-01 21:52:55 +0200

    hello world files

---
+++ src/main/java/com/HelloWorldApplication.java
@@ -0,0 +1,29 @@
+package com;
+
+import io.dropwizard.Application;
+import io.dropwizard.setup.Bootstrap;
+import io.dropwizard.setup.Environment;
+
+public class HelloWorldApplication extends Application<HelloWorldConfiguration> {
+
+    public static void main(final String[] args) throws Exception {
+        new HelloWorldApplication().run(args);
+    }
+
+    @Override
+    public String getName() {
+        return "HelloWorld";
+    }
+
+    @Override
+    public void initialize(final Bootstrap<HelloWorldConfiguration> bootstrap) {
+        // TODO: application initialization
+    }
+
+    @Override
+    public void run(final HelloWorldConfiguration configuration,
+                    final Environment environment) {
+        // TODO: implement application
+    }
+
+}
